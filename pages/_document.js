94206ca83b8c02997afb7de2df15ab5cd0717997
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-05-31 03:23:20 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-         <Head>
-   
+        <Head>
+    
+          <noscript>
+            <img height="1" width="1" style={{display: 'none'}}
+                 src={`https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1`} 
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
